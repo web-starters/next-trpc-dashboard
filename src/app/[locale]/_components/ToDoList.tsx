5618afcd0e30471ff0ed9e8a1d580154fd37c4b0
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-01 17:42:41 +0100

    added server and client side calls

--- src/app/[locale]/_components/ToDoList.tsx
+++ src/app/[locale]/_components/ToDoList.tsx
@@ -5,5 +5,11 @@ import { trpc } from '../../_trpc/client';
 export default function ToDoList() {
   const getTodos = trpc.getTodos.useQuery();
 
-  return <div>{JSON.stringify(getTodos.data)}</div>;
+  return (
+    <div>
+      <p>Client Side Todos</p>
+
+      <p>{JSON.stringify(getTodos.data)}</p>
+    </div>
+  );
 }
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,6 @@
 import { getDictionary } from '@/lib/dictionaries';
 import { type Locale } from '@/i18n-config';
+import { serverTrpc } from '../_trpc/serverClient';
 
 import { Heading } from '@/components/atoms/heading';
 import ToDoList from './_components/ToDoList';
@@ -10,10 +11,17 @@ interface Props {
 
 export default async function Page({ params }: Props) {
   const dict = await getDictionary(params.locale);
+  const todos = await serverTrpc.getTodos();
 
   return (
     <div className="max-w-3xl mx-auto p-6">
       <Heading>{dict['greeting']}</Heading>
+
+      <div>
+        <p>Server Side Todos</p>
+
+        <p>{JSON.stringify(todos)}</p>
+      </div>
 
       <ToDoList />
     </div>
