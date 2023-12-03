e98c10ffef17eb5fcf83ae7d789d6d888ea81ae6
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-03 19:08:16 +0100

    changed env validation

--- src/env.mjs
+++ src/env.mjs
@@ -8,9 +8,18 @@ export const env = createEnv({
    */
   server: {
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
-    DATABASE_URL: z.string().url(),
-    NEXTAUTH_SECRET: z.string(),
-    NEXTAUTH_URL: z.string().url(),
+    DATABASE_URL: z
+      .string()
+      .url()
+      .refine(str => !str.includes('YOUR_MYSQL_URL_HERE'), 'You forgot to change the default URL'),
+    NEXTAUTH_SECRET: process.env.NODE_ENV === 'production' ? z.string() : z.string().optional(),
+    NEXTAUTH_URL: z.preprocess(
+      // This makes Vercel deployments not fail if you don't set NEXTAUTH_URL
+      // Since NextAuth.js automatically uses the VERCEL_URL if present.
+      str => process.env.VERCEL_URL ?? str,
+      // VERCEL_URL doesn't include `https` so it cant be validated as a URL
+      process.env.VERCEL ? z.string() : z.string().url(),
+    ),
     GITHUB_ID: z.string(),
     GITHUB_SECRET: z.string(),
   },
