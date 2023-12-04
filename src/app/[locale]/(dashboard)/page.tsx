246ff542a12d5fa188ca21834ac7d2ee4ef97d77
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-04 20:26:02 +0100

    removed session

--- src/app/[locale]/(dashboard)/page.tsx
+++ src/app/[locale]/(dashboard)/page.tsx
@@ -1,8 +1,6 @@
-import Link from 'next/link';
 import { getTranslations } from 'next-intl/server';
 
 import { api } from '@/trpc/server';
-import { getServerAuthSession } from '@/server/auth';
 
 import { Heading } from '@/components/atoms/heading';
 import ToDoList from './_components/ToDoList';
@@ -10,19 +8,10 @@ import ToDoList from './_components/ToDoList';
 export default async function Page() {
   const t = await getTranslations('Index');
   const todos = await api.todo.getTodos.query();
-  const session = await getServerAuthSession();
 
   return (
     <div className="max-w-3xl mx-auto p-6">
       <Heading>{t('title')}</Heading>
-
-      <div>
-        <p>{session && <span>Logged in as {session.user?.name}</span>}</p>
-
-        <Link href={session ? '/api/auth/signout' : '/api/auth/signin'}>
-          {session ? 'Sign out' : 'Sign in'}
-        </Link>
-      </div>
 
       <div>
         <p>Server Side Todos</p>
