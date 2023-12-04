e8e54e121a0276922050c41ab3b3e98bf8f067b6
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-04 21:57:26 +0100

    styling

--- src/app/[locale]/signin/layout.tsx
+++ src/app/[locale]/signin/layout.tsx
@@ -1,4 +1,7 @@
 import { type ReactNode } from 'react';
+import { redirect } from 'next/navigation';
+
+import { getServerAuthSession } from '@/server/auth';
 
 import { Icons } from '@/components/atoms/icons';
 import Footer from '@/components/organisms/Footer';
@@ -7,14 +10,18 @@ interface Props {
   children: ReactNode;
 }
 
-export default function AuthLayout({ children }: Props) {
+export default async function AuthLayout({ children }: Props) {
+  const session = await getServerAuthSession();
+
+  if (session) redirect('/');
+
   return (
     <>
-      <header>
-        <Icons.logo className="w-20 h-6" />
+      <header className="flex justify-center p-5">
+        <Icons.logo className="w-40 h-9" />
       </header>
 
-      <main>{children}</main>
+      <main className="flex flex-col justify-center max-w-md mx-auto p-5">{children}</main>
 
       <Footer />
     </>
