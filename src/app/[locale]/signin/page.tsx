fc099ca343fbc1f8f78f05f16c293f9307004d17
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-06 20:21:04 +0100

    added dynamic metadata

--- src/app/[locale]/signin/page.tsx
+++ src/app/[locale]/signin/page.tsx
@@ -4,6 +4,14 @@ import { getTranslations } from 'next-intl/server';
 import { Heading } from '@/components/atoms/heading';
 import SignInProviders from './_components/SignInProviders';
 
+export async function generateMetadata() {
+  const t = await getTranslations('signin');
+
+  return {
+    title: t('title'),
+  };
+}
+
 export default async function Page() {
   const providers = await getProviders();
   const t = await getTranslations('signin');
