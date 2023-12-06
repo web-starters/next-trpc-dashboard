8629a05513bd19814f583e44608f7dd64357e3a5
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-06 21:58:10 +0100

    changed description

--- src/configs/site-config.ts
+++ src/configs/site-config.ts
@@ -2,6 +2,6 @@ export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: 'next-trpc-dashboard',
-  description: 'Next.js dashboard with tRPC',
+  description: 'Next.js dashboard starter with tRPC',
   author: 'Łukasz Warchoł <coded-bear>',
 };
