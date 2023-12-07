a7f0f701976b7e3cced5fe3f77eec8bb3563134c
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-07 18:50:20 +0100

    added data table

--- src/app/[locale]/(dashboard)/page.tsx
+++ src/app/[locale]/(dashboard)/page.tsx
@@ -1,8 +1,17 @@
 import { getTranslations } from 'next-intl/server';
+import { type ColumnDef } from '@tanstack/react-table';
 
 import { api } from '@/trpc/server';
+import { type RouterOutputs } from '@/trpc/shared';
 
 import { Heading } from '@/components/atoms/heading';
+import DataTable from '@/components/organisms/DataTable';
+
+const columns: ColumnDef<RouterOutputs['todo']['getOne']>[] = [
+  { accessorKey: 'id', header: 'ID' },
+  { accessorKey: 'name', header: 'Name' },
+  { accessorKey: 'createdBy', header: 'Created by' },
+];
 
 export default async function Page() {
   const t = await getTranslations('homepage');
@@ -12,7 +21,7 @@ export default async function Page() {
     <div className="max-w-7xl mx-auto">
       <Heading>{t('title')}</Heading>
 
-      <p>{JSON.stringify(todos)}</p>
+      <DataTable columns={columns} data={todos} />
     </div>
   );
 }
