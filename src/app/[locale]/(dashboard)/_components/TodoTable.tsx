4194d830dc57477bc66f7dcfd6cd3768ac07ea8a
Łukasz Warchoł <lukaszwarchol.dev@gmail.com>
2023-12-10 21:53:39 +0100

    added remove todo

--- src/app/[locale]/(dashboard)/_components/TodoTable.tsx
+++ src/app/[locale]/(dashboard)/_components/TodoTable.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
 import { MoreHorizontal } from 'lucide-react';
 import { type ColumnDef } from '@tanstack/react-table';
 
+import { api } from '@/trpc/react';
 import { type RouterOutputs } from '@/trpc/shared';
 
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
+import { AlertDialog, AlertDialogTrigger } from '@/components/ui/alert-dialog';
 import { Button } from '@/components/atoms/button';
 import {
   DropdownMenu,
@@ -15,6 +18,7 @@ import {
 } from '@/components/molecules/dropdown-menu';
 import DataTable from '@/components/organisms/DataTable';
 import FormDialogTemplate from '@/components/templates/FormDialogTemplate';
+import AlertDialogTemplate from '@/components/templates/AlertDialogTemplate';
 import TodoForm from './TodoForm';
 
 interface Props {
@@ -22,6 +26,11 @@ interface Props {
 }
 
 export default function TodoTable({ data }: Props) {
+  const router = useRouter();
+  const removeTodo = api.todo.remove.useMutation({
+    onSuccess: () => router.refresh(),
+  });
+
   const columns: ColumnDef<RouterOutputs['todo']['getOne']>[] = [
     { accessorKey: 'id', header: 'ID' },
     { accessorKey: 'name', header: 'Name' },
@@ -36,19 +45,29 @@ export default function TodoTable({ data }: Props) {
 
         return (
           <Dialog>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost" className="w-8 h-8 p-0">
-                  <span className="sr-only">Open menu</span>
-                  <MoreHorizontal className="w-4 h-4" />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DialogTrigger asChild>
-                  <DropdownMenuItem>Update</DropdownMenuItem>
-                </DialogTrigger>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <AlertDialog>
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button variant="ghost" className="w-8 h-8 p-0">
+                    <span className="sr-only">Open menu</span>
+                    <MoreHorizontal className="w-4 h-4" />
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DialogTrigger asChild>
+                    <DropdownMenuItem>Update</DropdownMenuItem>
+                  </DialogTrigger>
+                  <AlertDialogTrigger asChild>
+                    <DropdownMenuItem>Remove</DropdownMenuItem>
+                  </AlertDialogTrigger>
+                </DropdownMenuContent>
+              </DropdownMenu>
+
+              <AlertDialogTemplate
+                title={`Are you sure you want to remove ${item.name}`}
+                handleSubmit={() => removeTodo.mutate({ id: item.id })}
+              />
+            </AlertDialog>
 
             <FormDialogTemplate title="Update todo">
               <TodoForm itemToUpdate={item.id} values={{ name: item.name }} />
